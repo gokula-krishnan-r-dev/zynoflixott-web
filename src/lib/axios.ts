f0a18000c56d8bc2f4f9c181c875400cca77f209
gokula <gokulakrishnanr812@gmail.com>
2025-04-30 11:11:13 +0530

    Update API server URL in axios configuration to switch from a development endpoint to the production endpoint.

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -9,8 +9,8 @@ console.log(process.env.NEXT_PUBLIC_API_URL, "api");
 
 // const SERVER = "http://localhost:8080";
 
- const SERVER = "http://4.213.224.84:8080";
-//const SERVER = "https://api.zynoflixott.com";
+ //const SERVER = "http://4.213.224.84:8080";
+const SERVER = "https://api.zynoflixott.com";
 
 const axios = Axios.create({
   baseURL: `${SERVER}/api`,
