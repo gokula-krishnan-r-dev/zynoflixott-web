c9ba44afeb6907941c9db3b1e0109555361377e1
gokula <gokulakrishnanr812@gmail.com>
2025-05-13 20:04:45 +0530

    Update SendGrid email configuration in .env.local to use the live email address for production. This change ensures that emails are sent from the correct domain for improved deliverability.

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -18,9 +18,9 @@ const page = () => {
           Your browser does not support the video tag.
         </video>
         {/* <div className="hero-bg-gradient "></div> */}
-        <div className="max-w-screen-xl z-50 m-0 sm:m-10 rounded-3xl flex justify-center flex-1 ">
+        <div className="max-w-screen-xl z-30 m-0 sm:m-10 rounded-3xl flex justify-center flex-1 ">
           <div className="lg:w-1/2 xl:w-5/12 p-6  rounded-3xl  sm:p-12">
-            <div className="mt-12 flex flex-col items-center">
+            <div className="pt-12 flex flex-col items-center">
               <h1 className="text-2xl xl:text-3xl text-white font-extrabold">
                 Sign up
               </h1>
