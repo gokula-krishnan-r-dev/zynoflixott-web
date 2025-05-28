55b09153d96f4b832c3d4ba34104cff9b16376fb
gokula <gokulakrishnanr812@gmail.com>
2025-05-28 22:02:27 +0530

    new

--- src/app/(default)/create-live-stream/page.tsx
+++ src/app/(default)/create-live-stream/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from "next";
 import LiveStreamForm from "@/components/form/live-stream-form";
-import { isLogin } from "@/lib/user";
+import { isLogin, userId } from "@/lib/user";
 import Link from "next/dist/client/link";
 
 export const metadata: Metadata = {
@@ -9,16 +9,16 @@ export const metadata: Metadata = {
 };
 
 export default function CreateLiveStreamPage() {
-    if (!isLogin) {
-        return (
-            <div className="">
-                <div className="flex flex-col items-center justify-center h-screen">
-                    <h1 className="text-2xl font-bold">Please login to create a live stream</h1>
-                    <Link href="/login" className="bg-blue-500 text-white px-4 py-2 rounded-md">Login</Link>
-                </div>
-            </div>
-        )
-    }
+    // if (isLogin) {
+    //     return (
+    //         <div className="">
+    //             <div className="flex flex-col items-center justify-center h-screen">
+    //                 <h1 className="text-2xl font-bold">Please login to create a live stream</h1>
+    //                 <Link href="/login" className="bg-blue-500 text-white px-4 py-2 rounded-md">Login</Link>
+    //             </div>
+    //         </div>
+    //     )
+    // }
     return (
         <div className="pt-20 bg-[#0f0f1a]">
             <LiveStreamForm />
