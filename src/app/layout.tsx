2436e0fbb93b50c00d2d63910b90a230ca838490
gokula <gokulakrishnanr812@gmail.com>
2025-06-06 18:47:38 +0530

    Update Google Tag Manager and Tawk.to embed scripts for improved tracking and support. Replace old IDs with new ones for analytics and chat functionality.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,14 +41,14 @@ export default function RootLayout({
       <body className={inter.className}>
 
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-R92MZPEMQD"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17143508737"></script>
         <script
           dangerouslySetInnerHTML={{
             __html: `
               window.dataLayer = window.dataLayer || [];
               function gtag(){dataLayer.push(arguments);}
               gtag('js', new Date());
-              gtag('config', 'G-R92MZPEMQD');
+              gtag('config', 'AW-17143508737');
             `
           }}
         />
@@ -61,7 +61,7 @@ export default function RootLayout({
               (function(){
                 var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
                 s1.async=true;
-                s1.src='https://embed.tawk.to/683d3870e8c2fb1910b281da/1isni8u81';
+                s1.src='https://embed.tawk.to/68395286321e0b190a733f75/1isfuknoh';
                 s1.charset='UTF-8';
                 s1.setAttribute('crossorigin','*');
                 s0.parentNode.insertBefore(s1,s0);
