8f6ebfa9b3ade3a1a38b523a685bfe0385408c48
gokula <gokulakrishnanr812@gmail.com>
2026-01-26 12:12:38 +0530

    Update Razorpay key in config.ts to live credentials for production environment, ensuring secure payment processing.

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,6 +1,6 @@
 export const videoRatio = " lg:h-[165px] h-[110px] w-[180px] lg:w-[299px]";
 
-export const ZRPKEY = "rzp_test_S6AcB6I8TQuoVM";
+export const ZRPKEY = "rzp_live_2wtNMTtIzCco0O";
 
 export const ZYNOPROD = "https://zynoflixott.blob.core.windows.net/zynoflix/";
 
