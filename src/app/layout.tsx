13acb156b64192111388fc99aadcc99f7ac4b7db
ajaycr <Ajay-CR@users.noreply.github.com>
2026-02-20 13:05:51 +0530

    Add Google Analytics script to RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,85 @@ export default function RootLayout({
     <html lang="en">
       <head>
       <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-RYRQECYM7L"
+          src="https://www.googletagmanager.com/gtag/js?id=G-"use client";
+import Header from "@/components/layout/header";
+import "./globals.css";
+
+import { Toaster, toast } from "sonner";
+import { AuthProvider } from "@/components/provider/AuthProvider";
+import Footer from "@/components/shared/footer";
+import Script from "next/script";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+      <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-"use client";
+import Header from "@/components/layout/header";
+import "./globals.css";
+
+import { Toaster, toast } from "sonner";
+import { AuthProvider } from "@/components/provider/AuthProvider";
+import Footer from "@/components/shared/footer";
+import Script from "next/script";
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <head>
+      <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-3T38F16FTX"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-3T38F16FTX');
+          `}
+        </Script>
+      </head>
+      <body>
+        <Header />
+        <AuthProvider>{children}</AuthProvider>
+        <Toaster richColors />
+        <Footer />
+      </body>
+    </html>
+  );
+}"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-3T38F16FTX');
+          `}
+        </Script>
+      </head>
+      <body>
+        <Header />
+        <AuthProvider>{children}</AuthProvider>
+        <Toaster richColors />
+        <Footer />
+      </body>
+    </html>
+  );
+}"
           strategy="afterInteractive"
         />
         <Script id="google-analytics" strategy="afterInteractive">
