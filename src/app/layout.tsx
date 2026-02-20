41419eb5d34c281e9a2224ae78571f008dbd393a
ajaycr <Ajay-CR@users.noreply.github.com>
2026-02-20 12:29:28 +0530

    Integrate Google Analytics into layout
    Added Google Analytics tracking script to the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+      <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-RYRQECYM7L"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-RYRQECYM7L');
+</script>
+      </head>
       <body>
         <Header />
         <AuthProvider>{children}</AuthProvider>
