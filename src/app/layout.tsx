2576429b7fba5ddbcbc34a935c411a537f6a727f
ajaycr <Ajay-CR@users.noreply.github.com>
2026-02-20 12:36:52 +0530

    Update Google Analytics script integration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,15 +14,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-      <!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-RYRQECYM7L"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-RYRQECYM7L');
-</script>
+      <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-RYRQECYM7L"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-RYRQECYM7L');
+          `}
+        </Script>
       </head>
       <body>
         <Header />
