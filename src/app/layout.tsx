326b48bcf9850015c1be58cda1ba4939f5f61529
ajaycr <Ajay-CR@users.noreply.github.com>
2026-02-20 17:18:21 +0530

    Integrate Google Tag Manager and remove GA script
    Removed Google Analytics script and added Google Tag Manager integration.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,32 +1,45 @@
-import Script from "next/script";
+"use client";
+
 import Header from "@/components/layout/header";
 import "./globals.css";
-
-import { Toaster, toast } from "sonner";
+import { Toaster } from "sonner";
 import { AuthProvider } from "@/components/provider/AuthProvider";
 import Footer from "@/components/shared/footer";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
       <body>
+        {/* Google Tag Manager */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-3T38F16FTX"
+          id="gtm-script"
           strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TS6RTR54');
+            `,
+          }}
         />
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            window.gtag = gtag;
-            gtag('js', new Date());
-            gtag('config', 'G-3T38F16FTX');
-          `}
-        </Script>
+
+        {/* Optional: NoScript fallback */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TS6RTR54"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Header />
         <AuthProvider>{children}</AuthProvider>
         <Toaster richColors />
