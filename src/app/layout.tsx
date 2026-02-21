1505194e2f18b3e91c1ce0b4b22261d80d8d846d
ajaycr <Ajay-CR@users.noreply.github.com>
2026-02-21 12:11:13 +0530

    Remove GTM script from RootLayout
    Removed Google Tag Manager script from the head section to improve layout structure.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,7 +5,6 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import { AuthProvider } from "@/components/provider/AuthProvider";
 import Footer from "@/components/shared/footer";
-import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -14,33 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head>
-        {/* GTM Script moved to Head for better tracking accuracy */}
-        <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-              })(window,document,'script','dataLayer','GTM-TS6RTR54');
-            `,
-          }}
-        />
-      </head>
       <body>
-        {/* NoScript fallback stays in Body */}
-        <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-TS6RTR54"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-
         <Header />
         <AuthProvider>{children}</AuthProvider>
         <Toaster richColors />
