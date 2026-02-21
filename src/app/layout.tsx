1e70653709cd8f5abf6d104e9924e48547a5c5a5
ajaycr <Ajay-CR@users.noreply.github.com>
2026-02-21 13:50:34 +0530

    Update Google Tag Manager ID in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
           j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
           'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-          })(window,document,'script','dataLayer','GTM-P7RJCDB2');`}
+          })(window,document,'script','dataLayer','GTM-TS6RTR54');`}
         </Script>
 
         {/* Facebook SDK */}
