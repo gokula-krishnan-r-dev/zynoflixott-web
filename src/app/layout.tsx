354f2458685564bf8994c98a655b567e357983c5
ajaycr <Ajay-CR@users.noreply.github.com>
2026-02-27 14:18:15 +0530

    Add Google Tag Manager script to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import "./globals.css";
 import { Toaster } from "sonner";
 import { AuthProvider } from "@/components/provider/AuthProvider";
 import Footer from "@/components/shared/footer";
+import Script from "next/script";
 
 export default function RootLayout({
   children,
@@ -14,9 +15,33 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        {/* Google Tags Removed */}
+        {/* Google Tag Manager Script */}
+        <Script
+          id="gtm-script"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-TS6RTR54');
+            `,
+          }}
+        />
       </head>
+
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TS6RTR54"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
         <Header />
         <AuthProvider>{children}</AuthProvider>
         <Toaster richColors />
